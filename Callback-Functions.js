993e74704980b10fca154c16ccad2aaf22663d35
Ahsan Shakeel <ahsanshakeel1994@gmail.com>
2020-06-28 22:09:30 +0500

    Practising

--- Callback-Functions.js
+++ Callback-Functions.js
@@ -1,6 +1,8 @@
 //When a function simply accepts another function as an argument, 
 //this contained function is known as a callback function. 
 
+/*****************************************************/
+
 function greeting(name) {
 	console.log(`Hello ${name}`)
 }
@@ -12,3 +14,19 @@ function name(firstname, lastname, callback) {
 }
 name("Ahsan", "Shakeel", greeting)
 
+/*****************************************************/
+
+function preparedfood(callback) {
+	setTimeout(() => {
+		console.log("Prepared Food")
+		callback("Food is Ready")
+	})
+}
+
+function mycallback(value) {
+	console.log("Value: ", value)
+}
+
+preparedfood(mycallback)
+
+/*****************************************************/
